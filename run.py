3e068457a71f28518e00989a4e1f5c365cda3933
Xag8031 <75270910+Xag8031@users.noreply.github.com>
2022-12-12 20:47:14 +0000

    Fixed start to run properly without *

--- run.py
+++ run.py
@@ -52,14 +52,12 @@ def move(event):
 #Need to add editor button
 #Need to Create a custom close button
 def start():
-    root = tk.Tk()
+    root = Tk()
     img = Image.open('./Images/Map-title.png').resize((32*20, 32*20))
     img = ImageTk.PhotoImage(img)
-    canvas = tk.Canvas(root, width=32*20, height=32*20)
+    canvas = Canvas(root, width=32*20, height=32*20)
     canvas.create_image(0, 0, anchor='nw', image=img)
-    Start = tk.Button(root, text='Start', width=40, height=5, command=lambda: middle())
-    print(root.winfo_width())
-    print(root.winfo_height())
+    Start = Button(root, text='Start', width=40, height=5, command=lambda: middle())
     canvas.pack()
     root.after(1000, lambda: Start.place(x=root.winfo_width()/5, y=root.winfo_height()/1.75))
     root.mainloop()
